06159c41149a33bf4d4e850a46539cbe723f5ea9
Gwen W <gwendolyn.whelan@gmail.com>
2026-05-18 09:21:10 -0400

    use FILE*-backed stdio structs

--- h/main.c
+++ h/main.c
@@ -18,17 +18,19 @@ struct g_out_f { struct g_out o; FILE*f;};
 #define f_i(x) ((struct g_in_f*)(x))->f
 #define f_o(x) ((struct g_out_f*)(x))->f
 
-static struct g *_putc(struct g*f, int c, struct g_out*o) { return putc(c, stdout), f; }
-static struct g* _flush(struct g*f, struct g_out*o) { fflush(stdout); return f; }
-static struct g*_getc(struct g*f, struct g_in*) { return g_core_of(f)->b = getc(stdin), f; }
-static struct g* _ungetc(struct g*f, int c, struct g_in*) { return g_core_of(f)->b = ungetc(c, stdin), f; }
-static struct g* _eof(struct g*f, struct g_in*) { return g_core_of(f)->b = feof(stdin), f; }
-struct g_in _g_stdin = { .getc = _getc, .ungetc = _ungetc, .eof = _eof, },
-            *g_stdin = &_g_stdin;
-struct g_out _g_stdout = { .putc = _putc, .flush = _flush, },
-             *g_stdout = &_g_stdout;
+static struct g *_putc(struct g*f, int c, struct g_out*o) { return putc(c, f_o(o)), f; }
+static struct g* _flush(struct g*f, struct g_out*o) { fflush(f_o(o)); return f; }
+static struct g*_getc(struct g*f, struct g_in*i) { return g_core_of(f)->b = getc(f_i(i)), f; }
+static struct g* _ungetc(struct g*f, int c, struct g_in*i) { return g_core_of(f)->b = ungetc(c, f_i(i)), f; }
+static struct g* _eof(struct g*f, struct g_in*i) { return g_core_of(f)->b = feof(f_i(i)), f; }
+static struct g_in_f _g_stdin = {{ .getc = _getc, .ungetc = _ungetc, .eof = _eof, }, NULL};
+struct g_in *g_stdin = (void*)&_g_stdin;
+static struct g_out_f _g_stdout = {{ .putc = _putc, .flush = _flush, }, NULL};
+struct g_out *g_stdout = (void*)&_g_stdout;
 
 int main(int argc, char const **argv) {
+ _g_stdin.f = stdin;
+ _g_stdout.f = stdout;
  struct g *f;
  for (f = g_ini(); *argv; f = g_strof(f, *argv++));
  for (f = g_push(f, 1, g_nil); argc--; f = gxr(f));
